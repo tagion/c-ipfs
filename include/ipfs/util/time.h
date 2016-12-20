b914745b476926a5bd0d4c254aed8bec51707b5c
Jose Marcial Vieira Bisneto <marcial.vieirab@gmail.com>
2016-12-20 20:38:31 -0300

    Removed redundant struct stime from util/time

--- include/ipfs/util/time.h
+++ include/ipfs/util/time.h
@@ -1,12 +1,6 @@
 #ifndef IPFS_TIME_H
     #define IPFS_TIME_H
 
-    struct stime {
-        time_t t;
-        struct timespec ts;
-    };
-
-    int get_gmttime(struct stime *st);
-    int ipfs_util_time_parse_RFC3339 (struct stime *st, char *s);
-    char *ipfs_util_time_format_RFC3339 (struct stime *st);
+    int ipfs_util_time_parse_RFC3339 (struct timespec *ts, char *s);
+    char *ipfs_util_time_format_RFC3339 (struct timespec *ts);
 #endif // IPFS_TIME_H
