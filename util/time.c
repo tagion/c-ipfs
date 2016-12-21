6b9d205ef2c3b09c4e0511d10303d83259fedcc6
Jose Marcial Vieira Bisneto <marcial.vieirab@gmail.com>
2016-12-21 07:14:21 -0300

    Some changes at util/time

--- util/time.c
+++ util/time.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#ifndef __USE_XOPEN
-    #define __USE_XOPEN
-#endif // __USE_XOPEN
-
-#ifndef __USE_ISOC11
-    #define __USE_ISOC11
-#endif // __USE_ISOC11
-
-#include <time.h>
 #include "ipfs/util/time.h"
 
 int ipfs_util_time_parse_RFC3339 (struct timespec *ts, char *s)
