250b88601a0c307a357ae830b2939d8a1411d85a
jmjatlanta <jmjatlanta@gmail.com>
2017-07-20 07:57:20 -0500

    Beginnings of bitswap

---
+++ bitswap/network.c
@@ -0,0 +1,8 @@
+/***
+ * This implements the BitswapNetwork. Members of this network can fill requests and
+ * smartly handle queues of local and remote requests.
+ */
+
+#include "ipfs/bitswap/network.h"
+
+
